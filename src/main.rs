7eab606b6df48bb4a61a78fc8826834869d84c07
alfredjstanley <alfredjstanley56@gmail.com>
2021-03-13 19:54:38 +0530

    lib added

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use minigrep::{run, Config};
+use std::{env, process};
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("problem parsing the argument: {}", err);
+        process::exit(1);
+    });
+
+    println!("searching for '{}'", config.query);
+    println!("In file '{}'", config.filename);
+
+    if let Err(e) = run(config) {
+        println!("Application error {}", e);
+        process::exit(1);
+    }
+}
